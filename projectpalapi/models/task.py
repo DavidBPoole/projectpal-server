fafe0d16603f4ff4847824d98d2bc99c43735ee4
David Poole <dbpoole7@gmail.com>
2024-02-14 23:07:15 -0600

    initialized setup, settings, models, migrations and loaded fixture data

---
+++ projectpalapi/models/task.py
@@ -0,0 +1,22 @@
+from django.db import models
+from .project import Project
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    ]
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
